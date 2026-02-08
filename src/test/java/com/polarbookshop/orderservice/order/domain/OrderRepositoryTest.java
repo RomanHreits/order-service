e05a520cd8bcd3d22abf89ea491f5001ca9a36e9
RomanHreits <romagreic@gmail.com>
2026-02-08 12:54:35 +0100

    updated build.gradle

--- src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryTest.java
+++ src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryTest.java
@@ -2,17 +2,18 @@ package com.polarbookshop.orderservice.order.domain;
 
 import com.polarbookshop.orderservice.config.DataConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import reactor.test.StepVerifier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @DataR2dbcTest
 @Import(DataConfig.class)
@@ -24,32 +25,39 @@ public class OrderRepositoryTest {
       and used the "integration" profile for defining the PostgreSQL container properties.
  */
     @Container
-    static PostgreSQLContainer<?> postresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
+    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
 
     @Autowired
-    OrderRepository orderRepository;
+    private OrderRepository orderRepository;
 
 //    The @DynamicPropertySource annotation is used to register dynamic properties that will be applied to the Spring application context.
     @DynamicPropertySource
     static void postgresqlProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.r2dbc.url", OrderRepositoryTest::r2dbcUrl);
-        registry.add("spring.r2dbc.username", postresql::getUsername);
-        registry.add("spring.r2dbc.password", postresql::getPassword);
-        registry.add("spring.flyway.url", postresql::getJdbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
     }
 
     private static String r2dbcUrl() {
         return String.format("r2dbc:postgresql://%s:%s/%s",
-                postresql.getHost(),
-                postresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
-                postresql.getDatabaseName());
+                postgresql.getHost(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgresql.getDatabaseName());
     }
 
     @Test
-    public void createRejectedOrder() {
-        Order order = OrderService.buildRejectedOrder("1234567890", 1);
-        StepVerifier.create(orderRepository.save(order))
-                .expectNextMatches(o -> o.status().equals(OrderStatus.REJECTED))
+    void findOrderByIdWhenNotExisting() {
+        StepVerifier.create(orderRepository.findById(394L))
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    @Test
+    void createRejectedOrder() {
+        var rejectedOrder = OrderService.buildRejectedOrder( "1234567890", 3);
+        StepVerifier.create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(order -> order.status().equals(OrderStatus.REJECTED))
                 .verifyComplete();
     }
 }
